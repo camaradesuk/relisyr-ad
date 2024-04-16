59361c19397fa3b8813ba68b132a8115ea33f524
charis-wong <charis.wong@ed.ac.uk>
2024-04-16 18:59:54 +0100

    init

---
+++ global.R
@@ -0,0 +1,17 @@
+library(shiny)
+library(shinythemes)
+library(shinyWidgets)
+library(DT)
+library(googlesheets4)
+library(dplyr)
+library(shinycssloaders)
+
+source('configure.R')
+
+log <- googlesheets4::read_sheet(sheetId, sheet="log")
+lastupdatetime <- max(log$UpdateTime)
+
+entityOfInterest <- googlesheets4::read_sheet(sheetId, sheet="entityOfInterest")
+
+diseaseOfInterest <- entityOfInterest[entityOfInterest$Type == "diseaseOfInterest", ]$Item
+
